f08b712cd3660553577ef7a95939309095049d74
Gagan <gaganvarni232@gmai.com>
2025-03-12 14:26:05 +0530

    3rd code psuh push

---
+++ src/PrintNormal.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class PrintNormal {
+
+    public static void main(String[] args){
+
+        System.out.print("Gagan Vatni same line");
+        System.out.print("Gagan Vatni same line");
+        System.out.print("Gagan Vatni same line");
+        System.out.print("Gagan Vatni same line");
+
+    }
+
+}
