557ba86f806e6398b7ab317dc76f9cea7dda9488
Gagan <gaganvarni232@gmai.com>
2025-03-12 14:17:18 +0530

    2nd push

---
+++ src/HelloWord.java
@@ -0,0 +1,13 @@
+public class HelloWord {
+    public static void main(String[] args){
+        // public static void main(string[] atgs)
+
+        System.out.println("hello 2nd program");
+        System.out.println("hello  run 2nd program");
+
+
+
+    }
+
+
+}
