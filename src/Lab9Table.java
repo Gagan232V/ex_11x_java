4bbd8f084954ba056aa85ab1e0b67259f31cff75
Gagan <gaganvarni232@gmai.com>
2025-03-16 17:22:48 +0530

    9th table

---
+++ src/Lab9Table.java
@@ -0,0 +1,16 @@
+public class Lab9Table {
+
+    public static void main(String[] args) {
+
+        int g = 10;
+        System.out.printf("print value of %d" ,g);
+
+        int h = 120;
+        System.out.println(" --- ");
+        System.out.printf("%d + %d" ,g,h);
+        
+
+ 
+
+    }
+}
